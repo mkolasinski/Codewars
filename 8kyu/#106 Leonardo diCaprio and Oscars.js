a471dc70bd1365b8b8fb521ff32f0ef0635e5427
m.kolasinski <m.kolasinski@poczta.fm>
2017-10-08 13:12:54 +0200

    - kata 8kyu

---
+++ 8kyu/#106 Leonardo diCaprio and Oscars.js
@@ -0,0 +1,5 @@
+function leo(oscar) {
+  if (oscar == 88) return "Leo finally won the oscar! Leo is happy";
+  if (oscar == 86) return "Not even for Wolf of wallstreet?!";
+  return oscar > 88 ? "Leo got one already!" : "When will you give Leo an Oscar?";
+}
