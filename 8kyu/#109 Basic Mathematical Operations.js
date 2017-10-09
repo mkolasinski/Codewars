d44dd356ccc02042cb040fb924f5eb1d59309474
m.kolasinski <m.kolasinski@poczta.fm>
2017-10-09 18:34:59 +0200

    - kata 8kyu

---
+++ 8kyu/#109 Basic Mathematical Operations.js
@@ -0,0 +1,14 @@
+function basicOp(operator, a, b) {
+  if (operator === '+') {
+    return a + b;
+  }
+  else if (operator === '-') {
+    return a - b;
+  }
+  else if (operator === '*') {
+    return a * b;
+  }
+  else if (operator === '/') {
+    return a / b;
+  }
+}
