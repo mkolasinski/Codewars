5cdc7fb2f6940f2e95e72b5929d7d1f2e23d8233
m.kolasinski <m.kolasinski@poczta.fm>
2017-10-11 11:53:26 +0200

    - kata 8kyu

---
+++ 8kyu/#114 Blood-Alcohol Content.js
@@ -0,0 +1,10 @@
+function bloodAlcoholContent(drinks, weight, sex, time){
+
+  if (sex == "female") {
+    r = 0.66;
+  } else if (sex == 'male') {
+    r = 0.73;
+  }
+  bac = ((drinks.ounces * drinks.abv) * 5.14 / weight * r) - 0.015 * time ;
+  return Number(bac.toFixed(4));
+}
