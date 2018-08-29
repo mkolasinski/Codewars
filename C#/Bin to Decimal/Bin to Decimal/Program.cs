58e5b8bd905cd4b1c7c696727853cb1347382e86
mkolasinski <m.kolasinski@poczta.fm>
2018-08-29 18:22:28 +0200

    Bin to Decimal

---
+++ C#/Bin to Decimal/Bin to Decimal/Program.cs
@@ -0,0 +1,16 @@
+﻿#region Usings
+
+using System;
+
+#endregion
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static int binToDec(string s)
+        {
+            return Convert.ToInt32(s, 2);
+        }
+    }
+}
