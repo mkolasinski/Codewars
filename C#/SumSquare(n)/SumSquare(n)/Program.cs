71243893e88a8a7ee75c9afddc14a6ffa71794bf
mkolasinski <m.kolasinski@poczta.fm>
2018-08-29 18:43:02 +0200

    SquareSum

---
+++ C#/SumSquare(n)/SumSquare(n)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Kata
+{
+    public static int SquareSum(int[] n)
+    {
+        int summ = 0;
+        foreach (int value in n)
+        {
+            summ = summ + value * value;
+        }
+        return summ;
+    }
+}
