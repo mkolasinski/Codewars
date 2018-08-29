f55310c056279fc741aba51ca31a8f91aa9a51e0
mkolasinski <m.kolasinski@poczta.fm>
2018-08-29 18:28:01 +0200

    Bus

---
+++ C#/Bus/Bus/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static int Number(List<int[]> peopleListInOut)
+    {
+        int result = 0;
+        foreach (int[] a in peopleListInOut)
+        {
+            result += a[0];
+            result -= a[1];
+        }
+        return result;
+    }
+}
