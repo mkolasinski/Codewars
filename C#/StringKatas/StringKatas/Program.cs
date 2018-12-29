44274c94bcfb0d410b215dd668d068cfd23edc39
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 21:55:52 +0100

    StringKatas

---
+++ C#/StringKatas/StringKatas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace StringKatas
+{
+    public class Program
+    {
+        public string ToCamelCase(string s)
+        {
+            var strings = s.Split(new[] { '-', '_' });
+            return $"{strings[0]}{UpperCaseFirstChars(strings)}";
+        }
+
+        private string UpperCaseFirstChars(string[] strings)
+        {
+            var x = new StringBuilder();
+            for (int i = 1; i < strings.Length; i++)
+            {
+                var chars = strings[i].ToCharArray();
+                chars[0] = Char.ToUpper(chars[0]);
+                var s = string.Join("", chars);
+                x.Append(s);
+            }
+
+            return x.ToString();
+        }
+    }
+}
