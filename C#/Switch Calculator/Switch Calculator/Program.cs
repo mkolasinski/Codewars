70f6e16f51c885624e4b3d6b436b38c5a5c28801
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 21:34:27 +0100

    Simple int calculator with using switch mode

---
+++ C#/Switch Calculator/Switch Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Switch_Calculator
+{
+    class Program
+    {
+        public static double SomeObject( double v1, double v2, char operation)
+        {
+            double result = 0;
+            
+            switch (operation)
+            {
+                case '+' : result = v1 + v2; break;
+                case '-' : result = v1 - v2; break;
+                case '/': result = v1 / v2; break;
+                case '*' : result = v1 * v2; break;
+                case '%': result = v1 % v2; break;
+                case '^' : result = Math.Pow(v1, v2); break;
+            }
+
+            return result;
+        }
+    }
+}
