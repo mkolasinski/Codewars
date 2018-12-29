b90bd318e61d0cf1b10c62a427636be3eeaf121b
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 22:29:13 +0100

    Finish Guess The Number Game

---
+++ C#/FinishGuessTheNumberGame/FinishGuessTheNumberGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FinishGuessTheNumberGame
+{
+    class Guesser
+    {
+        private int num;
+        private int lives;
+
+        public Guesser(int num, int lives)
+        {
+            this.num = num;
+            this.lives = lives;
+        }
+
+        public bool Guess(int n)
+        {
+            if (lives == 0)
+            {
+                throw new Exception();
+            }
+
+            return n == num ? true : new Func<bool>(() => { lives--; return false; })();
+        }
+    }
+}
