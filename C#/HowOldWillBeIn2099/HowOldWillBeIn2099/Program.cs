507a03169ce227d7873afd85296dd8aa65ce8257
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 22:17:01 +0100

    How Old Will Be In 2099?

---
+++ C#/HowOldWillBeIn2099/HowOldWillBeIn2099/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HowOldWillBeIn2099
+{
+    class Program
+    {
+        public static string Age(int birth, int yearToBe)
+        {
+            int numberOfYears = Math.Abs(yearToBe - birth);
+            return yearToBe == birth ? "You were born this year" : yearToBe > birth ? $"You are {numberOfYears} year{(numberOfYears == 1 ? "" : "s")}old."
+        }
+    }
+}
