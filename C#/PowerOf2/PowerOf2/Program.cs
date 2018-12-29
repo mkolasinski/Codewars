6b442fdca60e95216678fb2e3a23bdef238a16f6
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 22:24:30 +0100

    Power Of Two

---
+++ C#/PowerOf2/PowerOf2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace PowerOf2
+{
+    class Program
+    {
+        public static BigInteger[] PowerOfTwo(int n)
+        {
+            BigInteger[] array = new BigInteger[n + 1];
+            for ( int i = 0; i < n + 1; i++)
+            {
+                array[i] = BigInteger.Pow(2, i);
+            }
+
+            return array;
+        }
+    }
+}
