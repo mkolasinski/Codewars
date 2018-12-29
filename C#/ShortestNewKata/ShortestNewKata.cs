e3a911a5ea1d1928e4a67266531409d124fe34af
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 21:20:38 +0100

    New Shortest NewKata

---
+++ C#/ShortestNewKata/ShortestNewKata.cs
@@ -0,0 +1,17 @@
+﻿#region Usings
+
+using System;
+using System.Linq;
+
+#endregion
+
+namespace ShortestNewKata
+{
+    public class ShortestNewKata
+    {
+        public static int FindShort(string words)
+        {
+            return words.Split(' ').OrderByDescending(word => word.Length).Last().Lenght;
+        }
+    }
+}
