2c6841ccef36b8f72f24abda210676ccbd772602
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 21:29:22 +0100

    Hoop!

---
+++ C#/Hoop/Hoop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoop
+{
+    public class Program
+    {
+        public static string HoopCount (int n)
+        {
+            return n < 10 ? "Keep at it until you get it" : "Great, now move on to tricks!";
+        }
+    }
+}
