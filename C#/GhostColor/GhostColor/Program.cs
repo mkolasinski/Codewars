a3428e63fdb5f12ca06a94585ed6f24f39b78a7d
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 22:00:54 +0100

    Ghost Color

---
+++ C#/GhostColor/GhostColor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GhostColor
+{
+    class Program
+    {
+        Random random = new Random();
+        string[] color = { "white", "blue", "yellow", "green" };
+
+        public string GetColor()
+        {
+            return color[random.Next(0, 4)];
+        }
+    }
+}
