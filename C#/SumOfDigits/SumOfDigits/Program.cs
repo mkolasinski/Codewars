e9c98a1a1d843b737fb03306f4cddeb914d4c885
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 21:48:50 +0100

    SumOfDigits

---
+++ C#/SumOfDigits/SumOfDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        public static int DigitalRoot(int n)
+        {
+            int someValue = n.ToString().Select(x => int.Parse(x.ToString())).Sum();
+            return (someValue < 10) ? someValue : DigitalRoot(someValue);
+        }
+
+        public class Number
+        {
+            public int DigitalRoot(long n)
+            {
+                return (int)(1 + (n - 1) % 9);
+            }
+        }
+    }
+}
