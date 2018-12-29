4c845c5bf9ed07fc66c6a939b3e48ac59f95170c
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 21:42:42 +0100

    Seats in Theatre

---
+++ C#/Theatre/Theatre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Theatre
+{
+    class Program
+    {
+        public static int SeatsInTheater (int Cols, int Rows, int col, int row)
+        {
+            return ((Cols - (col - 1) * (Rows - row)));
+        }
+    }
+}
