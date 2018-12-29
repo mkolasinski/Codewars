e7ef28930d8febea3a817176543cea08ac480e8d
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 22:31:55 +0100

    Filtering Even Number

---
+++ C#/FilteringEvenNumbersBugFixes/FilteringEvenNumbersBugFixes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FilteringEvenNumbersBugFixes
+{
+    public class Program
+    {
+        public static List<int> FilterOddNumber( List<int> listOfNumbers)
+        {
+            return listOfNumbers.Where(n => n % 2 == 1).ToList();
+        }
+    }
+}
