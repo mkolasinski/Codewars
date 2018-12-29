a4666e8fecb3bf68fc98edfc580380effea8793e
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 22:35:57 +0100

    Barycenter???

---
+++ C#/Barycenter/Barycenter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Barycenter
+{
+    class Program
+    {
+        public static double[] BarTriang(
+            double[] a,
+            double[] b,
+            double[] c)
+        {
+            var x = Math.Round(((a[0] + b[] + c[]) / 3), 4);
+            var y = Math.Round(((a[1] + b[1] + c[1]) / 3), 4);
+
+            return new[] { x, y };
+        }
+    }
+}
