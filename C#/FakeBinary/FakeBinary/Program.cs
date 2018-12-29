0f8048f3a8f19b35cbeb5adc468f1cd21f741b6a
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 22:04:47 +0100

    Fake Binary

---
+++ C#/FakeBinary/FakeBinary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace FakeBinary
+{
+    class Program
+    {
+        public static string FakeBinary(string a)
+        {
+            return string.Concat(a.Select(c => Int32.Parse(c.ToString()) < 5 ? 0 : 1));
+        }
+    }
+}
