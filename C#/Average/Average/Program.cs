109fd1bccc9d843d0c09032184b188fe40ec7863
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 22:02:34 +0100

    Average

---
+++ C#/Average/Average/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Average
+{
+    class Program
+    {
+        public static double FindAverage(double[] array)
+        {
+            return (array.Length == 0) ? 0 : array.Average();
+        }
+    }
+}
