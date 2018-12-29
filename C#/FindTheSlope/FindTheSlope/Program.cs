456fb05b6b2f0e2abab7e3bb223f03a66129497b
mkolasinski <m.kolasinski@poczta.fm>
2018-12-29 22:08:05 +0100

    FindTheSlope

---
+++ C#/FindTheSlope/FindTheSlope/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindTheSlope
+{
+    class Program
+    {
+        public string slope(int[] points)
+        {
+            return points[2] == points[0] ? "undefined" : $"{(points[3] - points[1]) / (points[2] - points[0])}";
+        }
+    }
+}
