fd5c55c1eab6355ced6d05afb228c920931caaba
mkolasinski <m.kolasinski@poczta.fm>
2018-12-30 12:03:24 +0100

    Repeat String

---
+++ C#/Repeat String/Repeat String/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Repeat_String
+{
+    class Program
+    {
+        public static string repeatString(int n, string text)
+        {
+            return string.Concat(Enumerable.Repeat(text, n));
+        }
+    }
+}
