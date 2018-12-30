64d1955211229f01ae4d27b8b4ca6a0172237e97
mkolasinski <m.kolasinski@poczta.fm>
2018-12-30 11:19:15 +0100

    RemovingElements

---
+++ C#/RemoveingElements/RemoveingElements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace RemoveingElements
+{
+    class Program
+    {
+        public static object[] RemoveEveryOther(object[] array)
+        {
+            return array.Where((x, i) => { return i % 2 == 0; }).ToArray();
+        }
+    }
+}
