04c13f5fed48c1b462010a7001833aa596d589b4
mkolasinski <m.kolasinski@poczta.fm>
2018-12-30 11:56:31 +0100

    Reversing Words in a string

---
+++ C#/ReversingWordsInAString/ReversingWordsInAString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReversingWordsInAString
+{
+    class Program
+    {
+        public static string Reverse(string text)
+        {
+            var someArray = text.Split(' ');
+            Array.Reverse(someArray);
+
+            return string.Join(" ", someArray);
+        }
+    }
+}
