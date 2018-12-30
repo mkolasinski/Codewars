4e522844e4ad25f597118f05aae68e27b08ea3f0
mkolasinski <m.kolasinski@poczta.fm>
2018-12-30 11:17:04 +0100

    Pre Fizz Buzz Workout

---
+++ C#/PreFizzBuzzWorkout/PreFizzBuzzWorkout/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PreFizzBuzzWorkout
+{
+    class Program
+    {
+        public static int[] PreFizzBuzz(int n)
+        {
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = i + 1;
+            }
+            return array;
+        }
+    }
+}
