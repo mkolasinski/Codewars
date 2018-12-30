8657cb72a7542102575f0284cd552817943e0971
mkolasinski <m.kolasinski@poczta.fm>
2018-12-30 12:13:14 +0100

    ReturnNegative

---
+++ C#/ReturnNegative/ReturnNegative/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReturnNegative
+{
+    class Program
+    {
+        public static int Negative(int num)
+        {
+            return num > 0 ? -num : num; 
+        }
+    }
+}
