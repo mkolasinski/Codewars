0f3fa009570d359ea189c34da8c9606c033021bd
mkolasinski <m.kolasinski@poczta.fm>
2018-12-30 11:24:19 +0100

    Regular Ball Super Ball

---
+++ C#/Regular Ball Super Ball/Regular Ball Super Ball/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Regular_Ball_Super_Ball
+{
+    public class Ball
+    {
+        public string ballType { get; set; }
+
+        public Ball()
+        {
+            ballType = "regular";
+        }
+
+        public Ball(string ballType)
+        {
+            this.ballType = ballType;
+        }
+    }
+}
