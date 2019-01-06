79db2ba458e6607b0bcdefacea32753eafd89377
mkolasinski <m.kolasinski@poczta.fm>
2019-01-06 19:00:02 +0100

    What is Between

---
+++ C#/WhatIsBetween/WhatIsBetween/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace WhatIsBetween
+{
+    class Program
+    {
+        public static int[] Between(int a, int b)
+        {
+            return Enumerable.Range(a, b - a + 1).ToArray();
+        }
+    }
+}
