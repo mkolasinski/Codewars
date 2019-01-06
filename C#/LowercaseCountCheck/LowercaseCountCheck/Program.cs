a90a157ccd774d01b0fa3900f324d15aa5adca04
mkolasinski <m.kolasinski@poczta.fm>
2019-01-06 14:46:09 +0100

    Replace written tasks to main folder

---
+++ C#/LowercaseCountCheck/LowercaseCountCheck/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LowercaseCountCheck
+{
+    class Program
+    {
+        public static int LowercaseCountCheck(string text)
+        {
+            return Regex.Replace(text, @"[^a-z]+", "").Length;
+        }
+    }
+}
---
+++ C#/SmallestUnnusedId/SmallestUnnusedId/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace SmallestUnusedId
+{
+    public class Program
+    {
+        public static int NextId(int[] indexes)
+        {
+            int n = 0; 
+
+            for ( int i = 0; i <= indexes.Length; i++)
+            {
+                if ( !indexes.Any( element => element == i))
+                {
+                    n = i;
+                    break;
+                }
+            }
+        }
+    }
+}
