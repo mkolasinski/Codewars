cc24c53730c4dd40278f57608e4211679146ab01
mkolasinski <m.kolasinski@poczta.fm>
2019-01-06 19:54:35 +0100

    Clock in mirror

---
+++ C#/ClockInMirror/ClockInMirror/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClockInMirror
+{
+    class Program
+    {
+        public static string WhatIstheTime(string timeInMirror)
+        {
+            DateTime dateTime = DateTime.ParseExact(timeInMirror, "HH:mm", CultureInfo.InvariantCulture);
+
+            int hour = (int)dateTime.Hour;
+
+            int minute = (int)dateTime.Minute;
+
+            hour = 12 - hour;
+
+            minute = (60 - minute) % 60;
+
+            if ( minute != 0)
+            {
+                --hour;
+            } else
+            {
+                minute = 0;
+            }
+
+            if (hour <= 0) hour += 12;
+
+            return String.Format("{0:00}:{1:00}", hour, minute);
+
+
+        }
+    }
+}
