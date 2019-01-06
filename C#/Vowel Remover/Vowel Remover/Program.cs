5914168baf492359e5a78f60b4823fd9c133bd5d
mkolasinski <m.kolasinski@poczta.fm>
2019-01-06 18:48:30 +0100

    Vowel Remover

---
+++ C#/Vowel Remover/Vowel Remover/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Vowel_Remover
+{
+    class Program
+    {
+        public static string Shortcut(string input)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            return string.Concat(input.Where(x => Array.IndexOf(vowels, x) == -1));
+        }
+    }
+}
