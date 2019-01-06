8a797dc48c69be670a50e8bd697725d567bb48dc
mkolasinski <m.kolasinski@poczta.fm>
2019-01-06 19:06:28 +0100

    Add More Item To List Bug Fixes

---
+++ C#/AddMoreItemToListBugFixes/AddMoreItemToListBugFixes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddMoreItemToListBugFixes
+{
+    class Program
+    {
+        public static List<int> AddSomethingExtra(List<int> listOfNumbers)
+        {
+            List<int> result = new List<int>(listOfNumbers);
+                result.Add(111);
+            return result;
+        }
+    }
+}
