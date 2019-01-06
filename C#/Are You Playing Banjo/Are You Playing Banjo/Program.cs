560ba496c3de4db20dc5acb0080030dc61368c21
mkolasinski <m.kolasinski@poczta.fm>
2019-01-06 19:10:40 +0100

    AreYouPlayBanjo

---
+++ C#/Are You Playing Banjo/Are You Playing Banjo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Are_You_Playing_Banjo
+{
+    class Program
+    {
+        public static string AreYouPlayingBanjo(string name)
+        {
+            return name + (Char.ToLower(name[0]) == 'r' ? "plays" : "does not play") + "banjo";
+        }
+    }
+}
