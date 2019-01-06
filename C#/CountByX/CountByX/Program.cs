c507a2a7056059b57ea1fc7b0139e08088c32f28
mkolasinski <m.kolasinski@poczta.fm>
2019-01-06 19:15:56 +0100

    CountByX

---
+++ C#/CountByX/CountByX/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace CountByX
+{
+    class Program
+    {
+        public static int[] CountByX(int x, int b)
+        {
+            return Enumerable.Range(1, b).Select(v => v * x).ToArray();
+        }
+    }
+}
