b927ef8911f689cec3533148e86dfac7dd2475d9
mkolasinski <m.kolasinski@poczta.fm>
2019-01-06 14:36:20 +0100

    Smallest Unused Id in array

---
+++ SmallestUnnusedId/SmallestUnnusedId/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmallestUnnusedId
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
