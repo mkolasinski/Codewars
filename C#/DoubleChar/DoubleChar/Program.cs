98a724078d72854d5b70177d0f7465a8326e2c8e
mkolasinski <m.kolasinski@poczta.fm>
2019-01-06 19:38:02 +0100

    DoubleChar

---
+++ C#/DoubleChar/DoubleChar/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace DoubleChar
+{
+    class Program
+    {
+        public static string DoubleChar(string text)
+        {
+            return string.Concat(text.Select(x => x.ToString() + x.ToString()));
+        }
+    }
+}
