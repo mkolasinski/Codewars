eb1a682e2c6f046818520b6c4ffbb83ab0990eac
mkolasinski <m.kolasinski@poczta.fm>
2019-01-06 14:52:04 +0100

    Square or square root

---
+++ C#/SquareOrSquareRoot/SquareOrSquareRoot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace SquareOrSquareRoot
+{
+    public class Program
+    {
+        public static int[] SquareOrSquareRoot(int[] arr)
+        {
+            return arr.Select(x => Convert.ToInt32(Math.Sqrt(x) - Math.Floor(Math.Sqrt(x)) > 0 ? Math.Pow(x, 2) : Math.Sqrt(x))).ToArray();
+        }
+    }
+}
